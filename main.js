31ecf2bd76b14dda916cc362cb408fb2a62e2188
Angy98-code <81024074+Angy98-code@users.noreply.github.com>
2021-11-19 12:22:47 +0100

    WIP remove tags

--- main.js
+++
@@ -1,7 +0,0 @@
-// the global variable recipes contains an array of recipes objects
-//2
-//console.log("main.js", recipes);
-//console.log("oneCardHtml", oneCardHtml(recipes[0]));
-
-placeCards();
-placeIngredients();
