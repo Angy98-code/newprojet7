860339293e652febb53b611617a7a9571a9a6a49
Angy98-code <81024074+Angy98-code@users.noreply.github.com>
2021-11-29 12:33:25 +0100

    WIP : input of the ingredients

--- js/searchIngredients.js
+++ js/searchIngredients.js
@@ -1,3 +1,4 @@
+//debugger;
 //----------toutes les variables globales
 let arrayDesIngredientsSelectionnes = []; // variable globale de tous les ingrédients sélectionnés (array de strings) ingredientsSelected
 //---------fin des variables globales
@@ -11,8 +12,9 @@ let arrayDesIngredientsSelectionnes = []; // variable globale de tous les ingré
 // - insère le html généré dans parentNode
 // - insère la recherche par ingrédient//
 // - point d'entrée (appelée dans main.js)
-
+//A0-A2
 function placeIngredients() {
+  //debugger;
   // liste des ingrédients dans le panneau de recherche
   const allIngredients = laListeDesIngredients();
   const parentNode = document.querySelector(".ingredientslist");
@@ -24,11 +26,20 @@ function placeIngredients() {
   // liste des tags ingredients selectionnés
   addIngredientsClickListener();
 }
+// todo :
+// 1- déclarer une fonction drawIngredients avec un paramètre ingredients (array de strings)
+// 2- migrer ligne 20 à 25 dans la fonction drawIngredients
+// 3- dans placeIngredients() appeler drawIngredients() avec pour paramètre allIngredients
+// 4- vérifier que la liste des ingrédients s'affiche comme avant
 
+// suite partie 4
+
+// fin suite partie 4
 //----------function traitement données
 // laListeDesIngredients retourne un array de strings contenant tous les ingrédients
-
+//A1
 function laListeDesIngredients() {
+  //debugger;
   let arrayListIngredients = [];
   recipes.forEach((recipe) => {
     recipe.ingredients.forEach((ingredient) => {
@@ -44,8 +55,10 @@ function laListeDesIngredients() {
 
 //----------addInputCallback() ajoute une callback sur l'évênement input désigné par cssSelector
 //callback
+//pas utilisé pour le moment
 
 function addInputCallback(cssSelector, saisieCB) {
+  //debugger;
   const nodeChampInputIngredients = document.querySelector(cssSelector);
 
   nodeChampInputIngredients.addEventListener("input", function (event) {
@@ -66,22 +79,39 @@ function addInputCallback(cssSelector, saisieCB) {
 // 5) sur click sur 1 ingrédient, appeler selectIngredient puis utiliser ingredientsTagsHtml pour afficher les tags dans le Dom (inline donc je vais utiliser les span)
 
 //------ selectIngredient() ajoute un ingrédient à l'array des ingrédients selectionnés
+// au click sur un ingrédient, cet ingrédient est ajouté à l'array
+//B1
 function selectIngredient(ingredient) {
+  //debugger;
   console.log(
     "arrayDesIngredientsSelectionnes avant push : ",
     arrayDesIngredientsSelectionnes
   );
   arrayDesIngredientsSelectionnes.push(ingredient);
+  ////////////////////////////////////////////////:WIP
+  //fonctionnementModalDesIngredients()
+
+  // const modalIngredient = document.querySelector(".ingredientsopen");
+  // modalIngredient.style.display = "none";
+  // const buttonIngredient = document.querySelector(".chooseingredients");
+  // buttonIngredient.style.display = "block";
+  // //
+  ///////////////////////////////////////////////////////
   console.log(
     "arrayDesIngredientsSelectionnes après push : ",
     arrayDesIngredientsSelectionnes
   );
+  // openIngredientsModal();
 }
 
 //----- addIngredientsClickListener() fonction qui ajoute un ingrédient à la sélection selectIngredient
+//A3 // B0 au click du addenentlistener la methode selectIngredient ()(push) est appelée puis
+//drawSelectedIngredientsTags();
 function addIngredientsClickListener() {
+  ///////debugger;
   const ulIngredients = document.querySelector(".ingredientslist");
   ulIngredients.addEventListener("click", (e) => {
+    //debugger;
     const recuperationIngredient = e.target.textContent;
     console.log(
       "recuperationIngredient avant appel fonction selectIngredient : ",
@@ -89,6 +119,7 @@ function addIngredientsClickListener() {
     );
     selectIngredient(recuperationIngredient);
     drawSelectedIngredientsTags();
+    closeIngredientsModal();
     console.log(
       "recuperationIngredient après appel selectIngredient : ",
       recuperationIngredient
@@ -99,6 +130,24 @@ function addIngredientsClickListener() {
 //----- 4) créer 1 fonction qui génére le code Html des tags d'ingrédients à partir d'arrayDesIngredientsSelectionnes (ingredientsTagsHtml)
 // 5) sur click sur 1 ingrédient, appeler selectIngredient puis utiliser drawSelectedIngredientsTags pour afficher les tags dans le Dom (inline donc je vais utiliser les span)
 
+// function drawSelectedIngredientsTags() {
+//   let tagIngredientHtml = "";
+//   const parentNode = document.querySelector(".placeringredientschoisis");
+//   arrayDesIngredientsSelectionnes.forEach((ingredient) => {
+//     tagIngredientHtml += `<div class="tagsingredientrow">
+//                             <span class="navbarresultchoosesingredients">
+//                             ${ingredient}
+//                             <i class="far fa-times-circle" id="boutonfermeturetag"></i>
+//                             </span>
+//                           </div>`;
+//   });
+//   parentNode.innerHTML = tagIngredientHtml;
+//   const spans = parentNode.querySelectorAll("span");
+//   spans.forEach((span) => {
+//     span.addEventListener("click", removeIngredientsClickListener);
+//   });
+// }
+//B2 // C1
 function drawSelectedIngredientsTags() {
   let tagIngredientHtml = "";
   const parentNode = document.querySelector(".placeringredientschoisis");
@@ -111,9 +160,12 @@ function drawSelectedIngredientsTags() {
                           </div>`;
   });
   parentNode.innerHTML = tagIngredientHtml;
-  const spans = parentNode.querySelectorAll("span");
-  spans.forEach((span) => {
-    span.addEventListener("click", removeIngredientsClickListener);
+  // add click handler in i tag
+  //pas exécuté dans B
+  //debugger;
+  const iTags = parentNode.querySelectorAll("i");
+  iTags.forEach((iTag) => {
+    iTag.addEventListener("click", removeIngredientsClickListener);
   });
 }
 
@@ -123,16 +175,74 @@ function drawSelectedIngredientsTags() {
 // 2 : dans la cb du click supprimer remove() l'ingredient clique arraySelect.... //
 // 3 : dans la cb appeler la fonction drawSelectedIngredientsTags
 
-const removeIngredientsClickListener = (e) => {
-  console.log("removeIngredientsClickListener", this, e);
+// const removeIngredientsClickListener = (e) => {
+//   console.log("removeIngredientsClickListener", this, e);
 
-  const recuperationTagASupprimer = e.target.textContent;
-  arrayDesIngredientsSelectionnes.remove(recuperationTagASupprimer);
+//   const recuperationTagASupprimer = e.target.textContent;
+//   arrayDesIngredientsSelectionnes.remove(recuperationTagASupprimer);
+//   drawSelectedIngredientsTags();
+// };
+
+//c0
+const removeIngredientsClickListener = (e) => {
+  // get clicked ingredient name
+  //debugger;
+  let ingredient = e.target.parentNode.textContent.trim();
+  let idx = arrayDesIngredientsSelectionnes.indexOf(ingredient);
+  if (idx >= 0) {
+    arrayDesIngredientsSelectionnes.splice(idx, 1);
+  }
   drawSelectedIngredientsTags();
 };
 
-//***** 4ème partie fermeture ouverture de la liste des infrédients
-//css display none
-// toggle addevent
+//why don't use
+// bouton fermeture croix x
+// const btnClose = document.querySelector("#closebtn");
+// const modalbg = document.querySelector(".bground");
+// btnClose.addEventListener("click", function (event) {
+//   modalbg.style.display = "none";
+// });
+
+//***** 4ème partie fermeture ouverture de la liste des ingrédients
+// closeIngredientsModal() appelé dans addIngredientsClickListener()
+// html ligne 85 : onclick="openIngredientsModal(event)" sur button des ingrédients
+
+function closeIngredientsModal() {
+  const modalIngredient = document.querySelector(".ingredientsopen");
+  const buttonIngredient = document.querySelector(".chooseingredients");
+  modalIngredient.style.display = "none";
+  buttonIngredient.style.display = "block";
+}
+
+function openIngredientsModal() {
+  const modalIngredient = document.querySelector(".ingredientsopen");
+  const buttonIngredient = document.querySelector(".chooseingredients");
+  modalIngredient.style.display = "block";
+  buttonIngredient.style.display = "none";
+}
 
 //***** 5ème partie input dans la modal ingrédients qui affiche un choix d'ingrédients après 3 lettres
+
+// récupérer la valeur de l'input
+//input recherche dans la modal(ne cherche les infos seulement parmi les ingrédients)
+function handleIngredientsSearch() {
+  const ingredientsInputSearchInModal =
+    document.querySelector(".inputingredients");
+  //la liste dans laquel la recherche doit se faire
+
+  ingredientsInputSearchInModal.addEventListener("input", (e) => {
+    e.preventDefault();
+    const recuperationSearchInputIngredient = e.target.value;
+    console.log(recuperationSearchInputIngredient);
+
+    if (recuperationSearchInputIngredient.length > 2) {
+      const troisLettersIngredients =
+        recuperationSearchInputIngredient.length > 2;
+
+      console.log(troisLettersIngredients);
+    }
+  });
+  // à partir de la 3ème lettre vérifier les similitudes avec les autres éléments
+  //afficher seulement les ingrédients avec ces 3 lettres dans le bon ordre
+}
+handleIngredientsSearch();
